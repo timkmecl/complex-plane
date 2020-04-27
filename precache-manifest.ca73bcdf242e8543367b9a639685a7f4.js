d3baea4407ab49196ad3d947b7707e123799029c
Tim Kmecl <tim.kmecl.tk@gmail.com>
2020-04-27 10:50:51 +0200

    Updates

---
+++ precache-manifest.ca73bcdf242e8543367b9a639685a7f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e58ed7cb38f6dbb1884a952dd507a6e",
+    "url": "/complex-plane/index.html"
+  },
+  {
+    "revision": "09db77e1ddf470bb1cd8",
+    "url": "/complex-plane/static/css/main.8aa39ce7.chunk.css"
+  },
+  {
+    "revision": "4d2d3ee10844b3fc622f",
+    "url": "/complex-plane/static/js/2.5aec461d.chunk.js"
+  },
+  {
+    "revision": "acc02d26138c2bd46fc52e905f7735ff",
+    "url": "/complex-plane/static/js/2.5aec461d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09db77e1ddf470bb1cd8",
+    "url": "/complex-plane/static/js/main.90749fa0.chunk.js"
+  },
+  {
+    "revision": "ad1e3b7abdefa4fb98a8",
+    "url": "/complex-plane/static/js/runtime-main.e8204d5f.js"
+  }
+]);
